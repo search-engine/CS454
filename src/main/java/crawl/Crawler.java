89672b2010f60dcbc453da106e23ae6a890a1f77
ed12al <ed12al@hotmail.com>
2016-02-05 15:37:48 -0800

    first init

---
+++ src/main/java/crawl/Crawler.java
@@ -0,0 +1,12 @@
+package crawl;
+
+public class Crawler {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		for (String arg: args) {
+            System.out.println("Received argument: " + arg);
+        }
+	}
+
+}
