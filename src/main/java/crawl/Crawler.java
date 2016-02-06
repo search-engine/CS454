049320f6af47691c9cbd7647b0f5c4f0f1b5b23b
ed12al <ed12al@hotmail.com>
2016-02-05 16:07:08 -0800

    a

--- src/main/java/crawl/Crawler.java
+++ src/main/java/crawl/Crawler.java
@@ -1,12 +1,38 @@
 package crawl;
 
 public class Crawler {
-
-	public static void main(String[] args) {
-		System.out.println("hello");
+	private static String url;
+	private static int depth = -1;
+	private static boolean isExtraction = false;
+ 	public static void main(String[] args) {
 		for (String arg: args) {
-            System.out.println("Received argument: " + arg);
+            String[] arguments = arg.split(" ");
+            if(arguments[0].equals("d")){
+            	try{
+            		depth = Integer.parseInt(arguments[1]);
+            	}catch(Exception e){
+            		System.out.println("depth has to be an integer");
+            		System.exit(0);
+            	}
+            }else if(arguments[0].equals("u")){
+            	if(arguments.length>1&&arguments[1].length()!=0){
+            		url = arguments[1];
+            	}else{
+            		System.out.println("Please enter an URL to start crawling");
+            		System.exit(0);
+            	}
+            }else if(arguments[0].equals("e")){
+            	isExtraction = true;
+            }else{
+            	System.out.println("unknown argument: "+arg);
+            }
         }
+		if(url==null||depth<0){
+			System.out.println("Either missing argument for depth or URL");
+			System.exit(0);
+		}
+		System.out.println("Depth is "+depth+"; URL is "+url+"; isExtraction: "+isExtraction);
+		
 	}
 
 }
