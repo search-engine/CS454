b305d0ac54c582476d9002cab93fab497707cf6e
suresanand89 <suresanand@gmail.com>
2016-02-12 20:50:21 -0800

    download

--- src/main/java/crawl/Crawl.java
+++ src/main/java/crawl/Crawl.java
@@ -16,6 +16,7 @@ import org.jsoup.select.Elements;
 public class Crawl implements Callable<Set<String>>{
 	private String url;
 	private int depth;
+	private static final String path = System.getProperty("user.dir") + "/src/url/";
 
 	public Crawl(String url, int depth) {
 		this.url = url;
@@ -33,29 +34,16 @@ public class Crawl implements Callable<Set<String>>{
 				}
 			}
 			byte[] bytes = Jsoup.connect(url).maxBodySize(2000000).ignoreContentType(true).execute().bodyAsBytes();
-			String path = System.getProperty("user.dir") + "/src/";
 			Response response = Jsoup.connect(url).ignoreContentType(true).execute();
 			String content = response.contentType();
-			MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-			messageDigest.update(url.getBytes());
-			String encryptedString = new String(messageDigest.digest());
-			if(content.contains("application/pdf")) {
-				FileOutputStream fos = new FileOutputStream(path + encryptedString + ".pdf");
-				fos.write(bytes);
-				fos.close();
-			}
-			else if(content.contains("text/html")) {
-				FileOutputStream fos = new FileOutputStream(path + encryptedString + ".html");
-				fos.write(bytes);
-				fos.close();
-			}
-			else if(content.contains("image/jpeg")) {
-				FileOutputStream fos = new FileOutputStream(path + encryptedString + ".jpg");
+			String fname = url.replace("://", "_").replace("/", "_");
+			if(content.contains("text/html")) {
+				FileOutputStream fos = new FileOutputStream(path + fname + ".html");
 				fos.write(bytes);
 				fos.close();
 			}
 			else {
-				FileOutputStream fos = new FileOutputStream(path + encryptedString + ".txt");
+				FileOutputStream fos = new FileOutputStream(path + fname);
 				fos.write(bytes);
 				fos.close();
 			}
