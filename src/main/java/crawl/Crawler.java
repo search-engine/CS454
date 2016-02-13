3ea1c2473a1de297f82bc78a722705af16d70619
sridharharsha <malavaiah.sridhar@gmail.com>
2016-02-12 17:00:25 -0800

    can download links

--- src/main/java/crawl/Crawler.java
+++ src/main/java/crawl/Crawler.java
@@ -1,9 +1,20 @@
 package crawl;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.tika.Tika;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Crawler {
 	private static String url;
@@ -40,11 +51,37 @@ public class Crawler {
 		try {
 			Document doc = Jsoup.connect(url).userAgent("Mozilla").get();
 			String test = doc.html();
-			System.out.println(test);
+			
+			File origFile = new File(System.getProperty("user.dir")+"/firstCrawl.txt");
+			FileWriter fileWriter = new FileWriter(origFile);
+			fileWriter.write(test);
+			fileWriter.close();
+			
+//			HashMap<String, String> baseMapLinks = new HashMap<String, String>();
+			Elements links = doc.select("a[href]");
+			Set<String> linkSet = new HashSet<String>();
+			for(Element link : links){
+				linkSet.add(link.attr("abs:href"));
+			}
+			System.out.println("linkSet "+linkSet.size());
+			Set<String> depthLinkSet = new HashSet<String>();
+			for(String depthCrawl2 : linkSet){
+
+				Document depthDoc = Jsoup.connect(depthCrawl2).ignoreContentType(true).userAgent("Mozilla").get();
+				
+				Elements depthLinks = depthDoc.select("a[href]");
+				for(Element link : depthLinks){
+					
+					depthLinkSet.add(link.attr("abs:href"));
+				}
+			}
+			System.out.println("depth link set size "+depthLinkSet.size());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
 
 	}
 
