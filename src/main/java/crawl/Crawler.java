8fa4a489b0023e5be64916ee72fa1e984c336a60
ed12al <ed12al@hotmail.com>
2016-02-12 17:15:12 -0800

    meger

--- src/main/java/crawl/Crawler.java
+++ src/main/java/crawl/Crawler.java
@@ -1,35 +1,13 @@
 package crawl;
 
-<<<<<<< HEAD
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
-=======
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
->>>>>>> 017054d8a0d31fbcf0ba7c4184ff4b631088ff18
 
-import org.apache.tika.Tika;
 import org.jsoup.Jsoup;
-<<<<<<< HEAD
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-=======
-import org.jsoup.helper.HttpConnection;
-import org.jsoup.helper.HttpConnection.Response;
->>>>>>> 017054d8a0d31fbcf0ba7c4184ff4b631088ff18
 
 public class Crawler {
 	private static String url;
@@ -71,48 +49,24 @@ public class Crawler {
 		
 		System.out.println("Depth is "+depth+"; URL is "+url+"; isExtraction: "+isExtraction);
 		
-		HttpConnection conn = (HttpConnection) Jsoup.connect(url).ignoreContentType(true).ignoreHttpErrors(true);
+		Set<String> urls = crawl(url);	
+		for(String u: urls){
+			System.out.println(u);
+		}
+	}
+ 	
+	private static Set<String> crawl(String url2) {
+		Set<String> linkSet = new HashSet<String>();
 		try {
 			Document doc = Jsoup.connect(url).userAgent("Mozilla").get();
-			String test = doc.html();
-			
-			File origFile = new File(System.getProperty("user.dir")+"/firstCrawl.txt");
-			FileWriter fileWriter = new FileWriter(origFile);
-			fileWriter.write(test);
-			fileWriter.close();
-			
-//			HashMap<String, String> baseMapLinks = new HashMap<String, String>();
 			Elements links = doc.select("a[href]");
-			Set<String> linkSet = new HashSet<String>();
 			for(Element link : links){
 				linkSet.add(link.attr("abs:href"));
 			}
-			System.out.println("linkSet "+linkSet.size());
-			Set<String> depthLinkSet = new HashSet<String>();
-			for(String depthCrawl2 : linkSet){
-
-				Document depthDoc = Jsoup.connect(depthCrawl2).ignoreContentType(true).userAgent("Mozilla").get();
-				
-				Elements depthLinks = depthDoc.select("a[href]");
-				for(Element link : depthLinks){
-					
-					depthLinkSet.add(link.attr("abs:href"));
-				}
-			}
-			System.out.println("depth link set size "+depthLinkSet.size());
-			Response response = (Response) conn.execute();
-			byte[] res = response.bodyAsBytes();
-			
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-
-		
-		
-		
-		
+		return linkSet;
 	}
 
 }
