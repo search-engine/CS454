9e5fc773385ecf6a310282e727c89a5ec5ee95dc
suresanand89 <suresanand@gmail.com>
2016-03-04 15:56:28 -0800

    file

--- src/main/java/Index/Indexing.java
+++ src/main/java/Index/Indexing.java
@@ -1,8 +1,32 @@
 package Index;
 
+import java.io.File;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
 public class Indexing {
 	public static void main( String args[] ) {
-			
+		File f = null;
+	      File[] paths;
+	      try{     
+	          // create new file
+	          f = new File("/Users/anandsuresh/Downloads/en");
+	          
+	          // returns pathnames for files and directory
+	          paths = f.listFiles();
+	          List<File> files = (List<File>) FileUtils.listFiles(f, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+	          // for each pathname in pathname array
+	          for(File file:files)
+	          {
+	             // prints file and directory paths
+	             System.out.println(file.getCanonicalPath());
+	          }
+	       }catch(Exception e){
+	          // if any error occurs
+	          e.printStackTrace();
+	       }
 			   
 	}
 }
