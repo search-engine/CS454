a8c3e8c9f7c5c4a4820ab8f884d70645ed1f4a6f
suresanand89 <suresanand@gmail.com>
2016-03-04 16:48:44 -0800

    new

--- src/main/java/Index/Indexing.java
+++ src/main/java/Index/Indexing.java
@@ -2,6 +2,7 @@ package Index;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
@@ -10,19 +11,21 @@ public class Indexing {
 	public static void main( String args[] ) {
 
 		File f = null;
-	      File[] paths;
 	      try{     
 	          // create new file
 	          f = new File(System.getProperty("user.home")+"/extra/wiki-small");
-	          
-	          // returns pathnames for files and directory
-	          paths = f.listFiles();
 	          List<File> files = (List<File>) FileUtils.listFiles(f, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
 	          // for each pathname in pathname array
 	          for(File file:files)
 	          {
 	             // prints file and directory paths
-	             System.out.println(file.getCanonicalPath());
+	        	 Indexer.indexer(file.getCanonicalPath());
+	        	 
+	             //System.out.println(file.getCanonicalPath());
+	          }
+	          for(Map.Entry<String, IndexWords> word : Indexer.getALlTerms().entrySet()) {
+	        	  System.out.println(word.getKey());
+	        	  System.out.println(word.getValue().getDocument());
 	          }
 	       }catch(Exception e){
 	          // if any error occurs
